0a2bd27ceb61d0d3dd92a1a8479afebfcd408b1f
MuhammadUzair <Uzairsuleman786@gmail.com>
2021-10-19 19:44:40 +0500

    Add project files.

---
+++ CorePractice/Models/WorkingWithExcel/ExcelFileHeaders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorePractice.Models.WorkingWithExcel
+{
+    public class ExcelFileHeaders
+    {
+        public string Sr { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+        public string Age { get; set; }
+        public string Date { get; set; }
+        public string Id { get; set; }
+    }
+}
